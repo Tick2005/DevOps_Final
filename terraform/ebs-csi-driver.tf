46038c3daa6b0ee18fcfaca7d385cf2b913ce431
Huytran2k5 <tranduchuy2k5ne@gmail.com>
2026-04-14 13:35:46 +0700

    chore: update deployment, monitoring, and infrastructure configs

--- terraform/ebs-csi-driver.tf
+++ terraform/ebs-csi-driver.tf
@@ -60,27 +60,5 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   ]
 }
 
-# Storage Class cho gp3 (default)
-resource "kubernetes_storage_class_v1" "gp3" {
-  metadata {
-    name = "gp3"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
-  }
-
-  storage_provisioner    = "ebs.csi.aws.com"
-  allow_volume_expansion = true
-  reclaim_policy         = "Delete"
-  volume_binding_mode    = "WaitForFirstConsumer"
-
-  parameters = {
-    type      = "gp3"
-    encrypted = "true"
-    fsType    = "ext4"
-  }
-
-  depends_on = [
-    aws_eks_addon.ebs_csi_driver
-  ]
-}
+# Note: Storage Class will be created by terraform-monitoring module
+# after cluster is ready and kubectl is configured
