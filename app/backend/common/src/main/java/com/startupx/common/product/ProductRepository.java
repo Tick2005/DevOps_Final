50fc92dfb463bc3d863373d4f69c79b59889c6cf
PhanVanDuong <phanvanduong1223456@gmail.com>
2026-04-14 18:23:01 +0700

    advance and fix errors: crud product

--- app/backend/common/src/main/java/com/startupx/common/product/ProductRepository.java
+++ app/backend/common/src/main/java/com/startupx/common/product/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.startupx.common.product;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<ProductDocument, Long> {
+	List<ProductDocument> findAllByOrderByIdAsc();
 }
